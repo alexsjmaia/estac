132480d2efed0506255aaa9e54bbe318adae8d6a
Alex <37779247+alexsjmaia@users.noreply.github.com>
2022-09-19 15:53:39 -0300

    Update database.go

--- main/database/database.go
+++ main/database/database.go
@@ -8,7 +8,7 @@ import (
 
 // Conectar CONECTA AO BANCO DE DADOS
 func Conectar() (*sql.DB, error) {
-	stringConexao := "root:260803@/estac?charset=utf8&parseTime=True&loc=Local"
+	stringConexao := "root:root@/estac?charset=utf8&parseTime=True&loc=Local"
 	db, erro := sql.Open("mysql", stringConexao)
 	if erro != nil {
 		return nil, erro
